df23d9ea635ad86734ffb4266cb78da220170b10
Vijayendra21 <hvijayendra8@gmail.com>
2024-07-02 22:37:01 +0530

    new model arch

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -26,4 +25,49 @@ class myOCRModel(nn.Module):
         x = x.view(-1, 256 * 8 * 8)
         x = self.dropout(F.relu(self.fc1(x)))
         x = self.fc2(x)
+        return x
+    
+
+'''MODEL TO RUN -abhi'''
+
+class myOCRModel(nn.Module):
+    def __init__(self, num_classes):
+        super(myOCRModel, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.dropout1 = nn.Dropout(0.6)
+        self.dropout2 = nn.Dropout(0.4)
+        self.fc1 = nn.Linear(36864, 1024)
+        self.fc2 = nn.Linear(1024, 2048)
+        self.fc3 = nn.Linear(2048, num_classes)
+
+    def forward(self, x):
+
+        x=F.relu(self.conv1(x))
+        x=self.pool(self.bn1(x))
+        
+        x=F.relu(self.conv2(x))
+        x=self.pool(self.bn2(x))
+        
+        x=F.relu(self.conv3(x))
+        x=self.pool(self.bn3(x))
+        
+        x=F.relu(self.conv4(x))
+        x=self.pool(self.bn4(x))
+
+        x=x.view(-1, 36864)
+
+        x=F.relu(self.fc1(x))
+        x=self.dropout1(x)
+        x=F.relu(self.fc2(x))
+        x=self.dropout2(x)
+        x=self.fc3(x)
+
         return x
